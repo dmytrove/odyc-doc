c3f1c7887172adcad18ca457a1321bf22f6c3d13
achtaitaipai <charles.cailleteau@gmail.com>
2025-06-04 14:23:47 +0200

    feat links alternate

--- src/routes/[[lang]]/+layout.svelte
+++ src/routes/[[lang]]/+layout.svelte
@@ -3,8 +3,25 @@
 	import 'codemirror-theme-vars/base.css'
 	import { type Snippet } from 'svelte'
 	import '../../app.css'
+	import { defaultLang, languages } from '$lib'
+	import { page } from '$app/state'
 	let { children }: { children: Snippet } = $props()
+	let lang = page.params.lang ?? defaultLang
+	let base = page.url.host
+	let path = $derived(page.url.pathname.replace(new RegExp(`^\/${lang}`), ''))
 </script>
+
+<svelte:head>
+	{#each Object.entries(languages) as [code]}
+		{#if code !== lang}
+			<link
+				rel="alternate"
+				hreflang={code}
+				href="https://{base}{code !== defaultLang ? '/' + code : ''}{path}"
+			/>
+		{/if}
+	{/each}
+</svelte:head>
 
 <div class="bg-base-100">
 	{@render children()}
