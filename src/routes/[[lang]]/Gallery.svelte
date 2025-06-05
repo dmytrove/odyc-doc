16602175106cb890fc92fc2735a8ae0a250cb4d5
achtaitaipai <charles.cailleteau@gmail.com>
2025-06-05 11:13:04 +0200

    games gallery lang

--- src/routes/[[lang]]/Gallery.svelte
+++ src/routes/[[lang]]/Gallery.svelte
@@ -1,10 +1,11 @@
 <script lang="ts">
 	import { page } from '$app/state'
-	import { useTranslations } from '$lib'
+	import { getLangFromUrl, useTranslations } from '$lib'
 	import { ChevronLeft, ChevronRight } from '@steeze-ui/heroicons'
 	import { Icon } from '@steeze-ui/svelte-icon'
 	import EmblaCarousel from 'embla-carousel'
 	import { onMount } from 'svelte'
+	import games from '../../content/gallery.json'
 
 	let wrapper: HTMLElement | undefined = $state()
 	let carousel: ReturnType<typeof EmblaCarousel>
@@ -14,46 +15,23 @@
 			carousel = EmblaCarousel(wrapper, { loop: true })
 		}
 	})
+	const pageLang = getLangFromUrl(page.url)
 
 	const t = useTranslations(page.params.lang)
-
-	const games = [
-		{ img: 'cdlr.png', url: 'https://jjjbon.itch.io/codedelaroute' },
-		{ img: 'dodog.png', url: 'https://achtaitaipai.github.io/odyc-exemples/games/dodog.html' },
-		{ img: 'morning.png', url: 'https://achtaitaipai.github.io/odyc-exemples/games/matin.html' },
-		{ img: 'vroum.png', url: 'https://achtaitaipai.github.io/odyc-exemples/games/vroum.html' },
-		{
-			img: 'maison-hantee.png',
-			url: 'https://achtaitaipai.github.io/odyc-exemples/games/maison-hantee.html'
-		},
-		{
-			img: 'the-sweeper.png',
-			url: 'https://achtaitaipai.github.io/odyc-exemples/games/balayeur.html'
-		},
-		{ img: 'island.png', url: 'https://achtaitaipai.github.io/odyc-exemples/games/island.html' },
-		{ img: 'space-bar.png', url: 'https://jjjbon.itch.io/spacebar-odyc-beta' },
-		{ img: 'emily.png', url: 'https://goupilette.itch.io/emily-sest-perdue' },
-		{
-			img: 'john-wick.png',
-			url: 'https://achtaitaipai.github.io/odyc-exemples/games/john-wick.html'
-		},
-		{
-			img: 'mushroom.png',
-			url: 'https://achtaitaipai.github.io/odyc-exemples/games/mushroom.html'
-		}
-	]
 </script>
 
 <div class="overflow-hidden" bind:this={wrapper}>
 	<div class="flex gap-4 py-12 ps-8">
-		{#each games as { img, url }}
-			<a class="group min-w-0 shrink-0 grow-0" href={url} target="_blank">
-				<img
-					alt=""
-					src="/gallery/{img}"
-					class="h-[14rem] w-auto rotate-6 transition-transform duration-300 group-odd:-rotate-6 group-hover:scale-105 sm:h-[28rem]"
-				/>
-			</a>
+		{#each games as { img, url, lang }}
+			{#if !lang || lang === pageLang}
+				<a class="group min-w-0 shrink-0 grow-0" href={url} target="_blank">
+					<img
+						alt=""
+						src="/gallery/{img}"
+						class="h-[14rem] w-auto rotate-6 transition-transform duration-300 group-odd:-rotate-6 group-hover:scale-105 sm:h-[28rem]"
+					/>
+				</a>
+			{/if}
 		{/each}
 	</div>
 </div>
