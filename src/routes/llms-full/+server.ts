de849eff40559a9ab098d5a01ce9818516c7d34c
achtaitaipai <charles.cailleteau@gmail.com>
2025-06-10 10:15:24 +0200

    feat llms-full txt

---
+++ src/routes/llms-full/+server.ts
@@ -0,0 +1,17 @@
+export async function GET() {
+	const rawDocs = import.meta.glob('../../content/doc/*/en.md', {
+		query: '?raw',
+		import: 'default'
+	})
+	const docs = (await Promise.all(Object.values(rawDocs).map((loader) => loader()))) as string[]
+	const content = docs
+		.map((el) => el.replace(/<script\b[^>]*>[\s\S]*?<\/script>/gi, ''))
+		.join('\n---\n')
+
+	return new Response(content, {
+		headers: {
+			'Content-Type': 'text/plain',
+			'Content-Disposition': `inline; filename="llms-full"`
+		}
+	})
+}
