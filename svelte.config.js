4d7c05dd06f13d0cb35cb9d780695d3d0428a2d4
achtaitaipai <charles.cailleteau@gmail.com>
2025-06-12 11:29:54 +0200

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -8,18 +8,7 @@ const config = {
 	extensions: ['.svelte', ...mdsvexOptions.extensions],
 	preprocess: [vitePreprocess(), mdsvex(mdsvexOptions)],
 	kit: {
-		adapter: adapter(),
-		prerender: {
-			handleHttpError: ({ status, path, referrer, referenceType }) => {
-				if (status === 404 && path.startsWith('/pagefind/')) {
-					console.warn(`⚠️ Ignoring missing Pagefind file during prerender: ${path}`)
-					return 'ignore'
-				}
-
-				// Let all other errors behave normally
-				return 'fail'
-			}
-		}
+		adapter: adapter()
 	}
 }
 
